17f7422eaa3f4b5bab654d4ebd8bf870fbfc6c20
KANGSOONGOO <finegs@gmail.com>
2019-07-01 14:11:51 +0900

    new utility batch file added

--- bin/setmsys.bat
+++ bin/setmsys.bat
@@ -1,6 +1,13 @@
 @echo off
+SET MSYS_HOME=D:\Programs\msys64
+
+SET UNDOPATH_1=%PATH%
 
 call setMinGW.bat
+
+SET UNDOPATH=%PATH%
+SET PATH=%MSYS_HOME%\usr\bin;%MSYS_HOME%;%PATH%
+SET REDOPATH=%PATH%
 rem cd /D C:\Work\workspace\pjt01
 rem start /b cmd.exe
 rem start cmd.exe
--- bin/undoPath.bat
+++ bin/undoPath.bat
@@ -1,2 +1,6 @@
 @echo off
-SET PATH=%UNDO_PATH%
+IF NOT ("%UNDOPATH_1%") == ("") (
+	SET PATH=%UNDOPATH_1%
+) ELSE (
+	SET PATH=%UNDO_PATH%
+)
